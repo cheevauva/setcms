d05a878a0a79f48907cefa2d438f6e95845f0cf1
Fedor Kazin <japanxt@ya.ru>
2021-10-09 23:24:51 +0500

    master: Переработка управления ссылками

--- resources/routes.php
+++ resources/routes.php
@@ -1,20 +1,15 @@
 <?php
 
 $routes = [
-    ['GET', '/[a:module]/read/[a:id]', ['action' => 'read'], 'read'],
-    ['GET', '/[a:module]/edit/[a:id]', ['action' => 'edit'], 'edit'],
-    ['GET', '/admin/[a:module]/read/[a:id]', ['action' => 'read', 'section' => 'Admin'], 'admin_read'],
-    ['GET', '/admin/[a:module]/edit/[a:id]', ['action' => 'edit', 'section' => 'Admin'], 'admin_edit'],
-    ['GET', '/admin/[a:module]/create/', ['action' => 'create', 'section' => 'Admin'], 'admin_create'],
-    ['GET', '/admin/[a:module]/index/', ['action' => 'index', 'section' => 'Admin'], 'admin_index'],
     ['GET', '/', ['action' => 'index', 'module' => 'Posts'], 'home'],
-    ['GET', '/[a:module]/', ['action' => 'index'], 'index'],
-    ['GET', '/[a:module]', ['action' => 'index'], 'index_alt'],
-    ['GET', '/[a:module]/[a:action]', ['section' => 'Index']],
-    ['GET', '/admin/[a:module]/[a:action]', ['section' => 'Admin']],
-//    ['POST', '/[a:module]/[a:action]/[i:id]', []],
-    ['POST', '/[a:module]/save', ['action' => 'save'], 'save'],
-    ['POST', '/admin/[a:module]/save', ['action' => 'save', 'section' => 'Admin'], 'admin_save'],
+    ['GET', '/[a:module]/[a:action]/[a:id]', ['section' => 'Index'], 'action_record'],
+    ['GET', '/[a:module]/[a:action]', ['section' => 'Index'], 'action'],
+    ['POST', '/[a:module]/[a:action]/[a:id]', ['section' => 'Index'], 'do_action_record'],
+    ['POST', '/[a:module]/[a:action]', ['section' => 'Index'], 'do_action'],
+    ['GET', '/admin/[a:module]/[a:action]/[a:id]', ['section' => 'Admin'], 'action_record_admin'],
+    ['GET', '/admin/[a:module]/[a:action]/', ['section' => 'Admin'], 'action_admin'],
+    ['POST', '/admin/[a:module]/[a:action]/[a:id]', ['section' => 'Admin'], 'do_action_record_admin'],
+    ['POST', '/admin/[a:module]/[a:action]/', ['section' => 'Admin'], 'do_action_admin'],
 ];
 
 foreach (glob(__DIR__ . '/routes/*') as $file) {
--- resources/routes/users.php
+++ resources/routes/users.php
@@ -1,7 +1,2 @@
 <?php
 
-$routes[] = ['GET', '/Users/login', ['module' => 'Users', 'action' => 'login'], 'login'];
-$routes[] = ['POST', '/Users/login', ['module' => 'Users', 'action' => 'doLogin'], 'do_login'];
-$routes[] = ['GET', '/Users/registration', ['module' => 'Users', 'action' => 'registration'], 'registration'];
-$routes[] = ['POST', '/Users/registration', ['module' => 'Users', 'action' => 'doRegistration'], 'do_registration'];
-$routes[] = ['GET', '/Users/logout', ['module' => 'Users', 'action' => 'logout'], 'do_logout'];
