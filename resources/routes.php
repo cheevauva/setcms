c6e98e0293a99a75d0ca2f854e293936eb06d887
Фёдор Казин <japanxt@ya.ru>
2024-05-12 16:49:09 +0500

    master: Динамические контроллеры принимают разные типы запросов

--- resources/routes.php
+++ resources/routes.php
@@ -8,14 +8,14 @@ use SetCMS\Module\Post\PostPrivateController;
 $routes = [
     'home' => ['GET', '/', PostPublicController::toRoute()->index()],
     'home_admin' => ['GET', '/~', PostPrivateController::toRoute()->index()],
-    'action_record' => ['GET', '/[a:module]/[a:action]/[g:id]', CorePublicController::toRoute()->dynamicAction()],
-    'action' => ['GET', '/[a:module]/[a:action]', CorePublicController::toRoute()->dynamicAction()],
-    'do_action_record' => ['POST', '/[a:module]/[a:action]/[g:id]', CorePublicController::toRoute()->dynamicAction()],
-    'do_action' => ['POST', '/[a:module]/[a:action]', CorePublicController::toRoute()->dynamicAction()],
-    'action_record_admin' => ['GET', '/~/[a:module]/[a:action]/[g:id]', CorePrivateController::toRoute()->dynamicAction()],
-    'action_admin' => ['GET', '/~/[a:module]/[a:action]', CorePrivateController::toRoute()->dynamicAction()],
-    'do_action_record_admin' => ['POST', '/~/[a:module]/[a:action]/[g:id]', CorePrivateController::toRoute()->dynamicAction()],
-    'do_action_admin' => ['POST', '/~/[a:module]/[a:action]', CorePrivateController::toRoute()->dynamicAction()],
+    'action_record' => ['GET|POST|DELETE|HEAD|PUT|OPTIONS|PATCH', '/[a:module]/[a:action]/[g:id]', CorePublicController::toRoute()->dynamicAction()],
+    'action' => ['GET|POST|DELETE|HEAD|PUT|OPTIONS|PATCH', '/[a:module]/[a:action]', CorePublicController::toRoute()->dynamicAction()],
+    'do_action_record' => ['GET|POST|DELETE|HEAD|PUT|OPTIONS|PATCH', '/[a:module]/[a:action]/[g:id]', CorePublicController::toRoute()->dynamicAction()],
+    'do_action' => ['GET|POST|DELETE|HEAD|PUT|OPTIONS|PATCH', '/[a:module]/[a:action]', CorePublicController::toRoute()->dynamicAction()],
+    'action_record_admin' => ['GET|POST|DELETE|HEAD|PUT|OPTIONS|PATCH', '/~/[a:module]/[a:action]/[g:id]', CorePrivateController::toRoute()->dynamicAction()],
+    'action_admin' => ['GET|POST|DELETE|HEAD|PUT|OPTIONS|PATCH', '/~/[a:module]/[a:action]', CorePrivateController::toRoute()->dynamicAction()],
+    'do_action_record_admin' => ['POST|DELETE|HEAD|PUT|OPTIONS|PATCH', '/~/[a:module]/[a:action]/[g:id]', CorePrivateController::toRoute()->dynamicAction()],
+    'do_action_admin' => ['POST|DELETE|HEAD|PUT|OPTIONS|PATCH', '/~/[a:module]/[a:action]', CorePrivateController::toRoute()->dynamicAction()],
 ];
 
 foreach (glob(__DIR__ . '/routes/*') as $file) {
