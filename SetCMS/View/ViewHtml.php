09fedfa43e8e3c43a611637192fed649f5df955a
Fedor Kazin <japanxt@ya.ru>
2025-03-31 00:58:09 +0500

    master_ref_controller: наброски новой архитектуры (5)

--- SetCMS/View/ViewHtml.php
+++ SetCMS/View/ViewHtml.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace SetCMS\View;
 
-use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Laminas\Diactoros\ServerRequestFactory;
 use Laminas\Diactoros\Uri;
