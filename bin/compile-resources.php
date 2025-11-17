2a9482acfda59780d931f6f61aad03596d674eaa
Fedor Kazin <japanxt@ya.ru>
2025-11-18 01:24:18 +0500

    30: Компиляторы динамические

--- bin/compile-resources.php
+++ bin/compile-resources.php
@@ -5,11 +5,7 @@ declare(strict_types=1);
 require dirname(__DIR__) . '/bootstrap.php';
 
 $rootPath = \SetCMS\Bootstrap::instance()->rootPath();
-$paths = [
-    'resources/resources.php' => 'cache/resources.php',
-    'resources/themes.php' => 'cache/themes.php',
-    'resources/module/menu/adminMenu.php' => 'cache/module/menu/adminMenu.php',
-];
+$paths = require $rootPath . 'resources/compiler.php';
 
 foreach ($paths as $from => $to) {
     if (!is_dir(dirname($to))) {
